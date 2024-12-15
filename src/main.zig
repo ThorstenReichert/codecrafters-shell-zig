b9227720890b914cb96d084edb4b88bc7b57aba6
Thorsten Reichert <th.reichert@gmx.net>
2024-12-15 23:46:10 +0100

    allow user to exit REPL

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const Result = union(enum) { exit: u8, cont };
+
 fn nextCommand(reader: anytype, buffer: []u8) !?[]const u8 {
     const line = (try reader.readUntilDelimiterOrEof(buffer, '\n')) orelse return null;
 
@@ -10,7 +12,16 @@ fn nextCommand(reader: anytype, buffer: []u8) !?[]const u8 {
     }
 }
 
-pub fn main() !void {
+fn handleCommand(writer: anytype, command: []const u8) !Result {
+    if (std.mem.startsWith(u8, command, "exit 0")) {
+        return Result{ .exit = 0 };
+    } else {
+        try writer.print("{s}: command not found\n", .{command});
+        return Result{ .cont = {} };
+    }
+}
+
+pub fn main() !u8 {
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
 
@@ -24,7 +35,12 @@ pub fn main() !void {
 
         // TODO: Handle user input
         if (user_input) |command| {
-            try stdout.print("{s}: command not found\n", .{command});
+            const result = try handleCommand(stdout, command);
+
+            switch (result) {
+                .cont => {},
+                .exit => |code| return code,
+            }
         }
     }
 }
