ea7289dbf7fe7ab8f1f0ec6c8e19bed68dfc093c
Thorsten Reichert <th.reichert@gmx.net>
2024-12-15 22:27:30 +0100

    print shell symbol

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,9 @@ pub fn main() !void {
     // const stdout = std.io.getStdOut().writer();
     // try stdout.print("$ ", .{});
 
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("$ ", .{});
+
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
     const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
